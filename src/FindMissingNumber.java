e10974990610da88f1182de8eff61a15a5b5824e
akilaisuru <akilaisurukahingala@gmail.com>
2026-08-17 02:28:18 +0530

    Find missing Number

---
+++ src/FindMissingNumber.java
@@ -0,0 +1,24 @@
+//Find Missing Number (1 සිට N දක්වා අඩුවී ඇති අංකය සෙවීම)අරමුණ: Loop Logic සහ Simple Mathematical Formula එකතු කර ගැටලු විසඳීම
+//.Input: {1, 2, 4, 5, 6} (N = 6)Output: 3 (අඩු අංකය)Key Logic: 1 සිට N දක්වා එකතුව $N \times (N+1) / 2$ සූත්‍රයෙන් අරන්,
+//ඇරේ එකේ Actual Sum එක එකෙන් අඩු කිරීම.
+
+public class FindMissingNumber {
+    public static void main(String[] args) {
+        int [] arr={1, 2, 4, 5, 6};
+        int n = arr.length+1;
+
+        int expected_sum = n*(n+1)/2;
+
+        int actualSum =0;
+        for(int num:arr){
+            actualSum+=num;
+        }
+
+        int mising_number = expected_sum-actualSum;
+        System.out.println("mising number = "+mising_number);
+
+
+    }
+
+
+}
